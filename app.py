d113e508f3280b237a0d83cb018357e0e25cded2
Arie <arie.prastowo@gmail.com>
2024-01-03 17:10:57 +0700

    Separate data store

--- app.py
+++ app.py
@@ -1,56 +1,51 @@
 from flask import Flask, request
+from db import stores, items
+import uuid
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name": "My Wonderful Store",
-        "items": [
-            {
-                "name": "My Item",
-                "price": 15.99
-            }
-        ]
-    }
-]
+
 
 @app.get("/store")
 def get_stores():
-    return {"stores": stores}   
+    return {"stores": list(stores.values())}   
 
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-    return {"message": "store not found"}, 404
-
-@app.get("/store/<string:name>/item")
-def get_items_in_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"items": store["items"]}
-    return {"message": "store not found"}, 404
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {"message": "store not found"}, 404
 
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
     new_store = {
-        "name": request_data["name"],
-        "items": []
+        **store_data, "id": store_id
     }
-    stores.append(new_store)
+    stores[store_id] = new_store
     return new_store, 201
 
-@app.post("/store/<string:name>/item")
-def create_item_in_store(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {
-                "name": request_data["name"],
-                "price": request_data["price"]
-            }
-            store["items"].append(new_item)
-            return new_item, 201
-    return {"message": "store not found"}, 404
+@app.post("/item")
+def create_item_in_store():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "store not found"}, 404
+    item_id = uuid.uuid4().hex
+    new_item = {
+        **item_data, "id": item_id
+    }
+    items[item_id] = new_item
+    return new_item, 201
+
+@app.get("/item")
+def get_items():
+    return {"items": list(items.values())}
+
+@app.get("/item/<string:item_id>")
+def get_items_in_store(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "item not found"}, 404
